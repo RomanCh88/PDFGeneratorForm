f58eb364fbe2fe6ed8b5d1bab33b41aec6e5e80c
rchomko <romanmediapr@gmail.com>
2021-08-11 23:21:38 +0300

    updated libraries and added composer

--- generator.php
+++ generator.php
@@ -1,9 +1,25 @@
 <?php
-require('lib/fpdf/fpdf.php');
+use setasign\Fpdi\Fpdi;
+use setasign\Fpdf\Fpdf;
+// or for usage with TCPDF:
+// use setasign\Fpdi\Tcpdf\Fpdi;
 
-$pdf = new FPDF();
+// or for usage with tFPDF:
+// use setasign\Fpdi\Tfpdf\Fpdi;
+
+// setup the autoload function
+require_once('vendor/autoload.php');
+
+// initiate FPDI
+$pdf = new Fpdi();
+// add a page
 $pdf->AddPage();
-$pdf->SetFont('Arial','B',16);
-$pdf->Cell(40,10,'Hello World!');
-$pdf->Output();
+// set the source file
+$pdf->setSourceFile("Fantastic-Speaker.pdf");
+// import page 1
+$tplId = $pdf->importPage(1);
+// use the imported page and place it at point 10,10 with a width of 100 mm
+$pdf->useTemplate($tplId, 10, 10, 100);
+
+$pdf->Output();  
 ?>
